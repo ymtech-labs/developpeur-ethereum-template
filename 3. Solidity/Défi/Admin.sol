118ec20521d45a4181be5169c754400b26176616
younesmjl <younes.manjal@gmail.com>
2021-11-08 20:02:28 +0100

    ajout requireWorkflowStatus structuration du code

--- 3. Solidity/Défi/Admin.sol
+++ 3. Solidity/Défi/Admin.sol
@@ -8,78 +8,121 @@ import "./Common.sol";
 /// @author Younès Manjal 😎
 /// @notice Le contrat fait appelle au mapping whitelist et à l'enum WorkflowStatus du contrat Common.sol
 contract Admin is Common {
-
     /// @notice Ajout d'une adresse Ethereum à la liste blanche
     /// @param _address address Ethereum d'un utilisateur ayant le droit de faire une proposition et de voter.
     /// @dev On récupére le mapping whitelist qui est définit dans le contrat common.sol
-    function addAddressWhitelist(address _address) public onlyOwner {
-        require(whitelist[_address].isRegistered != true, "This address is already whitelisted !");
-        Voter memory voterUsers = Voter(true, false, 0);
-        whitelist[_address] = voterUsers;
-        //defaultWorkflowStatus = WorkflowStatus.RegisteringVoters;
+    function addAddressWhitelist(address _address)
+        public
+        onlyOwner
+        requireWorkflowStatus(
+            WorkflowStatus.RegisteringVoters,
+            "The recording session is no longer in progress"
+        )
+    {
+        require(
+            whitelist[_address].isRegistered != true,
+            "This address is already whitelisted !"
+        );
+        white[_address] isRegistered = true;
         emit VoterRegistered(_address);
         //emit WorkflowStatusChange(WorkflowStatus.RegisteringVoters, WorkflowStatus.RegisteringVoters);
     }
-    
+
     /// @notice Récupére un électeur de la liste blanche
     /// @param _address address Ethereum d'un utilisateur de la liste blanche.
-    function isWhitelisted(address _address) public view onlyOwner returns (Voter memory){
+    function isWhitelisted(address _address)
+        public
+        view
+        onlyOwner
+        returns (Voter memory)
+    {
         return whitelist[_address];
     }
-    
+
     /// @notice Démarrage de l'enregistrement de propositions
     /// @dev On modifie la valeur de l'enum WorkflowStatus via la variable defaultWorkflowStatus qui est définit dans le contract common.sol
     function startsRecordingProposals() public onlyOwner {
         defaultWorkflowStatus = WorkflowStatus.ProposalsRegistrationStarted;
-        emit WorkflowStatusChange(WorkflowStatus.RegisteringVoters, WorkflowStatus.ProposalsRegistrationStarted);
+        emit WorkflowStatusChange(
+            WorkflowStatus.RegisteringVoters,
+            WorkflowStatus.ProposalsRegistrationStarted
+        );
     }
-    
+
     /// @notice On arrête la session de l'enregistrement de propositions
     /// @dev On modifie la valeurs de l'enum WorkflowStatus via la variable defaultWorkflowStatus qui est définit dans le contract common.sol
-    function stopRecordingProposals() public onlyOwner {
-        require(
-            defaultWorkflowStatus == WorkflowStatus.ProposalsRegistrationStarted, 
+    function stopRecordingProposals()
+        public
+        onlyOwner
+        requireWorkflowStatus(
+            WorkflowStatus.ProposalsRegistrationStarted,
             "There are no proposal registration sessions in progress"
-        );
+        )
+    {
         defaultWorkflowStatus = WorkflowStatus.ProposalsRegistrationEnded;
-        emit WorkflowStatusChange(WorkflowStatus.ProposalsRegistrationStarted, WorkflowStatus.ProposalsRegistrationEnded);
+        emit WorkflowStatusChange(
+            WorkflowStatus.ProposalsRegistrationStarted,
+            WorkflowStatus.ProposalsRegistrationEnded
+        );
     }
-    
+
     /// @notice Démmarage de la session de vote
     /// @dev On modifie la valeurs de l'enum WorkflowStatus via la variable defaultWorkflowStatus qui est définit dans le contract common.sol
-    function startsVotingSession() public onlyOwner {
-        require(
-            defaultWorkflowStatus != WorkflowStatus.ProposalsRegistrationStarted, 
+    function startsVotingSession()
+        public
+        onlyOwner
+        requireWorkflowStatus(
+            WorkflowStatus.ProposalsRegistrationEnded,
             "The voting registration session is still in progress"
-        );
+        )
+    {
         defaultWorkflowStatus = WorkflowStatus.VotingSessionStarted;
-        emit WorkflowStatusChange(WorkflowStatus.ProposalsRegistrationEnded, WorkflowStatus.VotingSessionStarted);
-    }                 
-    
-    /// @notice On arrête la session de vote
-    /// @dev Le modifier onlyVotingSessionIsStarted est définit dans le contrat common.sol
-    function stopVotingSession() public onlyOwner onlyVotingSessionIsStarted {
-        //onlyVotingSessionIsStarted
-        defaultWorkflowStatus = WorkflowStatus.VotingSessionEnded;
-        emit WorkflowStatusChange(WorkflowStatus.VotingSessionStarted, WorkflowStatus.VotingSessionEnded);
-    }
-    
-    /// @notice On comptabilise les votes
-    /// @dev Le modifier onlyVotingSessionIsStarted est définit dans le contrat common.sol
-    function voteTallying() public onlyOwner {
-        require(
-            defaultWorkflowStatus == WorkflowStatus.VotingSessionEnded, 
-            "The voting session is still in progress"
+        emit WorkflowStatusChange(
+            WorkflowStatus.ProposalsRegistrationEnded,
+            WorkflowStatus.VotingSessionStarted
         );
-        uint proposalsVoteCount = 0;
-        for (uint i = 0; i < proposals.length; i++) {
+    }
+
+    /// @notice On arrête la session de vote
+    /// @dev Le modifier requireWorkflowStatus est définit dans le contrat common.sol
+    function stopVotingSession()
+        public
+        onlyOwner
+        requireWorkflowStatus(
+            WorkflowStatus.VotingSessionStarted,
+            "There is no vote in progress"
+        )
+    {
+        defaultWorkflowStatus = WorkflowStatus.VotingSessionEnded;
+        emit WorkflowStatusChange(
+            WorkflowStatus.VotingSessionStarted,
+            WorkflowStatus.VotingSessionEnded
+        );
+    }
+
+    /// @notice On comptabilise les votes
+    /// @dev Le modifier requireWorkflowStatus est définit dans le contrat common.sol
+    function voteTallying()
+        public
+        onlyOwner
+        requireWorkflowStatus(
+            WorkflowStatus.VotingSessionEnded,
+            "The voting session is still in progress"
+        )
+    {
+        uint256 proposalsVoteCount = 0;
+
+        //TOS WARNING
+        for (uint256 i = 0; i < proposals.length; i++) {
             if (proposals[i].voteCount > proposalsVoteCount) {
-              proposalsVoteCount = proposals[i].voteCount;
-              winningProposalId = i;
+                proposalsVoteCount = proposals[i].voteCount;
+                winningProposalId = i;
             }
         }
         defaultWorkflowStatus = WorkflowStatus.VotesTallied;
-        emit WorkflowStatusChange(WorkflowStatus.VotingSessionEnded, WorkflowStatus.VotesTallied);
+        emit WorkflowStatusChange(
+            WorkflowStatus.VotingSessionEnded,
+            WorkflowStatus.VotesTallied
+        );
     }
-
 }
--- 3. Solidity/Défi/Common.sol
+++ 3. Solidity/Défi/Common.sol
@@ -1,10 +1,21 @@
 pragma solidity 0.8.9;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract Common is Ownable{
-    
-    mapping(address => Voter) whitelist;
-    
+contract Common is Ownable {
+    //STRUCTURE
+    //On créer un struct(objet) pour définir un proposition d'un utlisateur
+    struct Proposal {
+        string description;
+        uint256 voteCount;
+    }
+
+    //On créer un struct(objet) pour définir un élécteur
+    struct Voter {
+        bool isRegistered;
+        bool hasVoted;
+        uint256 votedProposalId;
+    }
+
     //Status du workflows;
     enum WorkflowStatus {
         RegisteringVoters,
@@ -14,45 +25,38 @@ contract Common is Ownable{
         VotingSessionEnded,
         VotesTallied
     }
-    
+
+    //VARIABLE GLOBAL
+    mapping(address => Voter) whitelist;
+    //On stocke les différentes propositions dans un tableau
+    Proposal[] public proposals;
+    //id de la proposition gagnante
+    uint256 winningProposalId;
     //On initialise une variable defaultWorkflowStatus pour utliser l'enum
-    WorkflowStatus public defaultWorkflowStatus = WorkflowStatus.RegisteringVoters;
-    
-            
+    WorkflowStatus public defaultWorkflowStatus =
+        WorkflowStatus.RegisteringVoters;
+
+    //EVENT TYPE
+    //évenement à renvoyer lorsqu'un utlisateur a voter
+    event VoterRegistered(address voterAddress);
+    event WorkflowStatusChange(
+        WorkflowStatus previousStatus,
+        WorkflowStatus newStatus
+    );
+    event ProposalRegistered(uint256 proposalId);
+    event Voted(address voter, uint256 proposalId);
+
+    //MODIFIERS
+    modifier requireWorkflowStatus(WorkflowStatus _statut,string memory _message) {
+        require(defaultWorkflowStatus == _statut, _message);
+        _;
+    }
+
     modifier onlyVotingSessionIsStarted() {
-         require(
-            defaultWorkflowStatus == WorkflowStatus.VotingSessionStarted, 
+        require(
+            defaultWorkflowStatus == WorkflowStatus.VotingSessionStarted,
             "There is no vote in progress"
         );
         _;
     }
-    
-    //On créer un struct(objet) pour définir un proposition d'un utlisateur
-    struct Proposal {
-        string description;
-        uint voteCount;
-    }
-    
-    //On stocke les différentes propositions dans un tableau
-    Proposal[] public proposals;
-    
-    //On créer un struct(objet) pour définir un élécteur   
-    struct Voter {
-        bool isRegistered;
-        bool hasVoted;
-        uint votedProposalId;
-    }
-    
-    //On stocke les élécteurs dans un tableau
-    Voter[] public voters;
-     
-    //id de la proposition gagnante
-    uint winningProposalId; 
-    
-    //évenement à renvoyer lorsqu'un utlisateur a voter
-    event VoterRegistered(address voterAddress); 
-    event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
-    event ProposalRegistered(uint proposalId);
-    event Voted (address voter, uint proposalId);
-
 }
