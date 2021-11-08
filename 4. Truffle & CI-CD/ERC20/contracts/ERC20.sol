a2a910cc4e8257583b5d17660ec27e2f59cf070c
younesmjl <younes.manjal@gmail.com>
2021-11-08 10:41:38 +0100

    Ajout tp ERC20

---
+++ 4. Truffle & CI-CD/ERC20/contracts/ERC20.sol
@@ -0,0 +1,9 @@
+pragma solidity 0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ERC20Token is ERC20 {
+    constructor(uint256 initialSupply) ERC20("ALYRA", "ALY") {
+        _mint(msg.sender, initialSupply);
+    }
+}
