6d45e7ddabf87fff71f569fbac6436be6a1fdcf3
Younès Manjal <younes.manjal@gmail.com>
2021-12-14 19:05:10 +0100

    add data read with web3

--- 6.Dapp/Introduction/index.js
+++ 6.Dapp/Introduction/index.js
@@ -1,12 +1,39 @@
-require("dotenv").config();
-const Web3 = require("web3");
+import dotenv from "dotenv";
+import Web3 from "web3";
+dotenv.config();
+
 const rpcURL = process.env.RPC;
 const web3 = new Web3(rpcURL);
 
+//Récupération Balance d'une adresse Ethereum
 web3.eth.getBalance(
   "0xb8c74A1d2289ec8B13ae421a0660Fd96915022b1",
   (err, wei) => {
-    balance = web3.utils.fromWei(wei, "ether"); // convertir la valeur en ether
-    console.log(balance);
+    let balance = web3.utils.fromWei(wei, "ether"); // convertir la valeur en ether
+    console.log("getBalance : ", balance);
   }
 );
+
+//Lecture d'un données d'un smart contact
+const ABI = [
+  {
+    inputs: [{ internalType: "uint256", name: "x", type: "uint256" }],
+    name: "set",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "get",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+    constant: true,
+  },
+];
+const SSaddress = "0xCbd43b4CF42101693689a1f9C201471d8f505E8f";
+const simpleStorage = new web3.eth.Contract(ABI, SSaddress);
+simpleStorage.methods.get().call((err, data) => {
+  console.log("SimpleStorage || get() || data : ", data);
+});
