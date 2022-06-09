f5b76ea3b4d32fba99ab1fda85d8f4cedaf5c141
ymtech <younes.manjal@gmail.com>
2022-06-09 10:52:56 +0200

    customize nft and add flatten contract

--- 8.NFT/tp-erc721/contracts/Bloke.sol
+++ 8.NFT/tp-erc721/contracts/Bloke.sol
@@ -4,21 +4,31 @@ pragma solidity 0.8.14;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Bloke is ERC721URIStorage {
+contract BlokeNFT is ERC721URIStorage {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
 
-  constructor() ERC721("Bloke", "BYM") {}
+  struct Bloke{
+    uint height;
+    bool hair;
+  }
 
-  function awardItem(address player, string memory tokenURI)
+	Bloke[] blokes;
+
+  constructor() ERC721("Bloke", "BYM") {
+    
+  }
+
+  function MintBloke(address player, string memory tokenURI, uint height, bool hair)
     public
     returns (uint256)
   {
+    blokes.push(Bloke(height, hair));
     uint256 newItemId = _tokenIds.current();
     _mint(player, newItemId);
     _setTokenURI(newItemId, tokenURI);
-
     _tokenIds.increment();
+    
     return newItemId;
   }
 }
